d539af776a72416aa0b2b74cf2eba5ae73b2726a
Tom Richter <tom.richter@outlook.com>
2018-10-26 11:31:55 -0400

    Blocked out refactor using Harmony

--- PulsarPluginLoader/Properties/AssemblyInfo.cs
+++ PulsarPluginLoader/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PulsarPluginLoader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PulsarPluginLoader.dll")]
+[assembly: AssemblyDescription("https://github.com/TomRichter/pulsar-plugin-loader")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PulsarPluginLoader")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Pulsar Plugin Loader")]
+[assembly: AssemblyCopyright("Copyright © Tom Richter 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]

d9809aa80b3b69bd7acc638536ffc1fcc5f74698
Tom Richter <tom.richter@outlook.com>
2018-10-26 11:34:02 -0400

    Fixed TS3 not connecting

--- PulsarPluginLoader/hooks/TS3Fix.cs
+++ PulsarPluginLoader/hooks/TS3Fix.cs
@@ -1,11 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Harmony;
 
 namespace PulsarPluginLoader.hooks
 {
-    class TS3Fix
+    [HarmonyPatch(typeof(PLVoiceChatManager))]
+    [HarmonyPatch("GetVoiceChatChannelName")]
+    internal class TS3Fix
     {
+        private static string Postfix(string __result)
+        {
+            return string.Concat(new string[]
+            {
+                "[",
+                PLXMLOptionsIO.Instance.CurrentOptions.GetStringValue("PhotonRegion") + " - ",
+                PLNetworkManager.Instance.VersionString.Substring(0, PLNetworkManager.Instance.VersionString.IndexOf('\n')),
+                PhotonNetwork.room.name,
+                "]"
+            }).Normalize();
+        }
     }
 }

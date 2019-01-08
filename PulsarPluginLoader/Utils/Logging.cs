287ea46a299690087e404e31edf5f83938b3e8db
Jai <flyfast.jai@gmail.com>
2019-01-08 16:46:08 +1030

    Write log to file

--- PulsarPluginLoader/Utils/Logging.cs
+++ PulsarPluginLoader/Utils/Logging.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.IO;
 
 namespace PulsarPluginLoader.Utils
 {
     public static class Logger
     {
+        private static StreamWriter Stream = new StreamWriter("Log.txt", false);
+
         public static void Info(string message)
         {
             Console.WriteLine($"[PPL] {message}");
+            Stream.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} {message}");
+            Stream.Flush();
         }
     }
 }

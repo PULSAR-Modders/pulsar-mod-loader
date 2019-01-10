d3d2b7d53c0cc3aea586954d6dc9dc6fc989da88
Tom Richter <tom.richter@outlook.com>
2019-01-09 20:27:33 -0500

    Fixed log location during unit tests

--- PulsarPluginLoader/Utils/Logging.cs
+++ PulsarPluginLoader/Utils/Logging.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 
 namespace PulsarPluginLoader.Utils
 {
     public static class Logger
     {
-        private static StreamWriter Stream = new StreamWriter("Log.txt", false);
+        private static string logPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Log.txt");
+        private static StreamWriter Stream = new StreamWriter(logPath, false);
 
         public static void Info(string message)
         {

6714cd04b2d8d7a80afe87742dc047382941a6c1
Tom Richter <tom.richter@outlook.com>
2019-06-23 02:05:30 -0400

    Added automatic UI toggle to /ss

--- PulsarPluginLoader/Chat/Commands/ScreenshotCommand.cs
+++ PulsarPluginLoader/Chat/Commands/ScreenshotCommand.cs
@@ -1,10 +1,7 @@
 ﻿using PulsarPluginLoader.Utilities;
 using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using UnityEngine;
 
 namespace PulsarPluginLoader.Chat.Commands
@@ -18,15 +15,15 @@ namespace PulsarPluginLoader.Chat.Commands
 
         public string Description()
         {
-            return "Saves a screenshot to disk.";
+            return "Saves a screenshot to disk.  Specifying \"ui\" will toggle the UI during capture.";
         }
 
         public string UsageExample()
         {
-            return $"/{CommandAliases()[0]}";
+            return $"/{CommandAliases()[0]} [ui]";
         }
 
-        public bool Execute(string bugDescription)
+        public bool Execute(string arg)
         {
             string baseDir = Environment.ExpandEnvironmentVariables(@"%localappdata%low\Leafy Games, LLC\PULSAR Lost Colony");
             Directory.CreateDirectory(baseDir);
@@ -42,13 +39,49 @@ namespace PulsarPluginLoader.Chat.Commands
             string screenshotPath = Path.Combine(outputDir, $"{levelID}_{visualType}_{timestamp}.png");
 
             // Take screenshot
-            ScreenCapture.CaptureScreenshot(screenshotPath);
-            Clipboard.Copy(screenshotPath);
+            bool shouldToggleUI = arg.ToLower().Trim() == "ui";
+            // Need a MonoBehavior to advance frames this way but chat commands can't be MonoBehaviors,
+            // so hijack the UI instance since it does derive from MonoBehavior.
+            PLInGameUI.Instance.StartCoroutine(WaitForScreenshot(screenshotPath, shouldToggleUI));
 
             Messaging.Echo(PLNetworkManager.Instance.LocalPlayer.GetPhotonPlayer(), $"Screenshot saved: {screenshotPath}");
             Messaging.Echo(PLNetworkManager.Instance.LocalPlayer.GetPhotonPlayer(), $"File path copied to clipboard.");
 
             return false;
+        }
+
+        private IEnumerator WaitForScreenshot(string screenshotPath, bool shouldToggleUI)
+        {
+            yield return TakeScreenshot(screenshotPath, shouldToggleUI);
+        }
+
+        private IEnumerator TakeScreenshot(string screenshotPath, bool shouldToggleUI)
+        {
+            if (shouldToggleUI)
+            {
+                ToggleUI();
+                yield return null;
+            }
+
+            ScreenCapture.CaptureScreenshot(screenshotPath);
+            yield return null;
+
+
+            if (shouldToggleUI)
+            {
+                ToggleUI();
+                yield return null;
+            }
+
+            Clipboard.Copy(screenshotPath);
+        }
+
+        private void ToggleUI()
+        {
+            if (PLInGameUI.Instance != null)
+            {
+                PLInGameUI.Instance.UIIsHidden = !PLInGameUI.Instance.UIIsHidden;
+            }
         }
     }
 }

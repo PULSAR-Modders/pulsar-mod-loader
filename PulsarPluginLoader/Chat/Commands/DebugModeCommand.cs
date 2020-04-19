00ef0538547a5bd816e4a9c95060b136932a8b4e
Dragon <1redyoung@gmail.com>
2020-04-19 19:47:18 -0400

    Created Toggle for Exception warning notifications, set default to false, added /DeBugMode Command to toggle.

---
+++ PulsarPluginLoader/Chat/Commands/DebugModeCommand.cs
@@ -0,0 +1,38 @@
+﻿using PulsarPluginLoader.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PulsarPluginLoader.Chat.Commands
+{
+    class DebugModeCommand : IChatCommand
+    {
+        public string[] CommandAliases()
+        {
+            if (bool.TryParse(PLXMLOptionsIO.Instance.CurrentOptions.GetStringValue("PPLDebugMode"), out bool result))
+            {
+                ExceptionWarningPatch.DebugMode = result;
+            }
+            return new string[] { "debugmode", "dbm"};
+        }
+
+        public string Description()
+        {
+            return "Toggles Exception notifications in-game";
+        }
+
+        public bool Execute(string arguments)
+        {
+            ExceptionWarningPatch.DebugMode = !ExceptionWarningPatch.DebugMode;
+            PLXMLOptionsIO.Instance.CurrentOptions.SetStringValue("PPLDebugMode", ExceptionWarningPatch.DebugMode.ToString());
+            Messaging.Notification(PLNetworkManager.Instance.LocalPlayer.GetPhotonPlayer(), $"PPLDebugMode is now {ExceptionWarningPatch.DebugMode}");
+            return false;
+        }
+
+        public string UsageExample()
+        {
+            return $"{CommandAliases()[0]}";
+        }
+    }
+}
--- PulsarPluginLoader/Utilities/ExceptionWarningPatch.cs
+++ PulsarPluginLoader/Utilities/ExceptionWarningPatch.cs
@@ -11,15 +11,17 @@ namespace PulsarPluginLoader.Utilities
         {
             Application.logMessageReceived += OnUnityLog;
         }
-
+        public static bool DebugMode = false;
         private static void OnUnityLog(string line, string stackTrace, LogType type)
         {
             if (type.Equals(LogType.Exception))
             {
                 string id = String.Format("{0:X}", DateTime.UtcNow.GetHashCode()).Substring(0, 7).ToUpper();
                 string msg = $"<color='#{ColorUtility.ToHtmlStringRGB(Color.red)}'>Exception!</color> {id}";
-
-                Messaging.Notification(PLNetworkManager.Instance.LocalPlayer.GetPhotonPlayer(), msg);
+                if (DebugMode)
+                {
+                    Messaging.Notification(PLNetworkManager.Instance.LocalPlayer.GetPhotonPlayer(), msg);
+                }
                 Logger.Info($"Exception ID: {id}");
             }
         }

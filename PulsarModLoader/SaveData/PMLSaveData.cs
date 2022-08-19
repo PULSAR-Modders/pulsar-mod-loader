6e43f2fdcdf1e33a6326fb16461106a3c9a633c4
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-08-19 16:38:27 -0700

    -Initial file merge -SaveDataManager Init added to PLGlobalStart

---
+++ PulsarModLoader/SaveData/PMLSaveData.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace PulsarModLoader.SaveData
+{
+    public abstract class PMLSaveData
+    {
+        public PulsarMod MyMod;
+        public abstract string Identifier();
+        public virtual uint VersionID => 0;
+        public abstract MemoryStream SaveData();
+        public abstract void LoadData(MemoryStream dataStream, uint VersionID);
+    }
+}

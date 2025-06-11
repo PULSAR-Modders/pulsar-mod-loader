a26561610f89f4806039fa2b362e6600317df04b
TheRealMesteven <mestevenyoutubing@gmail.com>
2025-06-11 23:57:43 +0100

    Badryuiner PrefabCache fix

---
+++ PulsarModLoader/Patches/FixPrefabCache.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace PulsarModLoader.Patches
+{
+    /// <summary>
+    /// Adding photonview objects to the PrefabCache sometimes causes an error.
+    /// This fix was made by Badryuiner.
+    /// </summary>
+    [HarmonyPatch]
+    internal static class FixPrefabCache
+    {
+        public static MethodBase TargetMethod()
+        {
+            var type = AccessTools.TypeByName("NetworkingPeer");
+            return AccessTools.Method(type, "DoInstantiate");
+        }
+
+        public static void Prefix(ExitGames.Client.Photon.Hashtable evData, PhotonPlayer photonPlayer, ref GameObject resourceGameObject)
+        {
+            if (resourceGameObject == null)
+            {
+                string text = (string)evData[0];
+                PhotonNetwork.PrefabCache.TryGetValue(text, out resourceGameObject);
+            }
+        }
+    }
+}
